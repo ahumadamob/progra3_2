8d984cb4bb5ae4660d423ee4c4ea9c1ada73b982
Mario Ahumada <ahumadamob@gmail.com>
2025-04-06 12:15:43 -0300

    Primer commit

---
+++ src/main/java/imb/progra2025/p3ro2da/P3ro2daApplication.java
@@ -0,0 +1,13 @@
+package imb.progra2025.p3ro2da;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3ro2daApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P3ro2daApplication.class, args);
+	}
+
+}
---
+++ src/test/java/imb/progra2025/p3ro2da/P3ro2daApplicationTests.java
@@ -0,0 +1,13 @@
+package imb.progra2025.p3ro2da;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P3ro2daApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
